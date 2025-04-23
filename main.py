5a1bd3563417124aa267d5e5f28cacac66f524c0
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2025-04-23 17:01:39 +0530

    Player

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
-import os
 
+Height = 800
+Width = 600
+
+
+# Player_Size = (60,60)
+# Player_Positon = (370,530)
 
 # Pygame Setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Space Invaders")
+screen = pygame.display.set_mode((Height, Width))
 clock = pygame.time.Clock()
 running = True
 
-player = pygame.image.load('./assets/Player.png')
+player = pygame.image.load("./assets/Player.png")
 
 while running:
+    player = pygame.transform.scale(player, (60,60))
+    screen.blit(player, (370,530))
+
+
+
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
-    
-
-pygame.display.flip()
-
-clock.tick(60) # Framerate
+           
+    clock.tick(60) # Framerate
 pygame.quit()
