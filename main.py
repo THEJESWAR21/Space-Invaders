01daf73a7626b2b6cb8431fd546497d1d43297ef
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2025-05-17 14:58:58 +0530

    Player-Movement

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import pygame
+from pygame.locals import *
+import os
 
-Height = 800
-Width = 600
-
-
-# Player_Size = (60,60)
-# Player_Positon = (370,530)
-
-# Pygame Setup
 pygame.init()
-pygame.display.set_caption("Space Invaders")
-screen = pygame.display.set_mode((Height, Width))
+
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+pygame.display.set_caption("Space Invaders")
+framerate = 60
+
+player = pygame.image.load(os.path.join('assets', 'Player.png'))
+
+player_width = player.get_width()
+player_height = player.get_height()
+
+player = pygame.transform.scale(player, (player_width * 0.4, player_height * 0.4))
+
+
+
+x_pos = 700
+y_pos = 600
+
 running = True
-
-player = pygame.image.load("./assets/Player.png")
-
 while running:
-    player = pygame.transform.scale(player, (60,60))
-    screen.blit(player, (370,530))
+
+    screen.fill(0)
+    screen.blit(player, (x_pos,y_pos))
 
 
-
-    pygame.display.flip()
+    # EXIT Functionality
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
-           
-    clock.tick(60) # Framerate
+            running = False
+
+    key_pressed_is = pygame.key.get_pressed()
+
+    if key_pressed_is[K_LEFT] or key_pressed_is[K_a]:
+        x_pos -= 5
+    if key_pressed_is[K_RIGHT] or key_pressed_is[K_d]:
+        x_pos += 5
+
+
+    clock.tick(framerate) # FrameRate
+    pygame.display.flip()
+
 pygame.quit()
